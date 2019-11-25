835b4e01e16751cdea0073afbfd64a028f56a49f
jimmy <jimmy.gout@hotmail.com>
2019-11-25 09:56:36 +0100

    Ajout de alert dans l'api

---
+++ src/Entity/Alert.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\AlertRepository")
+ */
+class Alert
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $designation;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $afficher;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $date_debut;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $date_fin;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDesignation(): ?string
+    {
+        return $this->designation;
+    }
+
+    public function setDesignation(string $designation): self
+    {
+        $this->designation = $designation;
+
+        return $this;
+    }
+
+    public function getAfficher(): ?bool
+    {
+        return $this->afficher;
+    }
+
+    public function setAfficher(bool $afficher): self
+    {
+        $this->afficher = $afficher;
+
+        return $this;
+    }
+
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->date_debut;
+    }
+
+    public function setDateDebut(?\DateTimeInterface $date_debut): self
+    {
+        $this->date_debut = $date_debut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTimeInterface
+    {
+        return $this->date_fin;
+    }
+
+    public function setDateFin(?\DateTimeInterface $date_fin): self
+    {
+        $this->date_fin = $date_fin;
+
+        return $this;
+    }
+}
