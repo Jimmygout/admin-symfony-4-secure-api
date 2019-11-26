d5af20b13556c38eb95d88f30293343d1a399f5c
jimmy <jimmy.gout@hotmail.com>
2019-11-26 12:23:33 +0100

    Ajout de Nouvelle à l'api

---
+++ src/Entity/Nouvelle.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\NouvelleRepository")
+ */
+class Nouvelle
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $designation;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $afficher;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lieux;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDesignation(): ?string
+    {
+        return $this->designation;
+    }
+
+    public function setDesignation(string $designation): self
+    {
+        $this->designation = $designation;
+
+        return $this;
+    }
+
+    public function getAfficher(): ?bool
+    {
+        return $this->afficher;
+    }
+
+    public function setAfficher(bool $afficher): self
+    {
+        $this->afficher = $afficher;
+
+        return $this;
+    }
+
+    public function getLieux(): ?string
+    {
+        return $this->lieux;
+    }
+
+    public function setLieux(string $lieux): self
+    {
+        $this->lieux = $lieux;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
